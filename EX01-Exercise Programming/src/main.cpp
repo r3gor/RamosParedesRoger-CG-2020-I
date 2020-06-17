7d5c2ecf618bd457995868df6d9dffaf31bb09a7
rogrp <yihsic@gmail.com>
2020-06-16 20:34:35 -0500

    add EX01

---
+++ EX01-Exercise Programming/src/main.cpp
@@ -0,0 +1,65 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 16 jun. 2020
+ *      Author: rogrp
+ */
+
+// Include standard headers
+#include <iostream>
+
+// Include GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+//sdsdsfghgj
+using namespace std;
+
+void display(double currentTime) {
+	glClearColor(.5f, 1.0f, .5f, 1.0f);
+	glClear( GL_COLOR_BUFFER_BIT);
+}
+
+int main( void ){
+	// Initialise GLFW
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); 	// Resizable option.
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(800, 800, "Ramos Paredes Roger Anthony", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		exit(EXIT_FAILURE);
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Check if the window was closed
+	while (!glfwWindowShouldClose(window)) {
+		display(glfwGetTime());
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+
+}
+
